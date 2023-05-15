41ce3414c12aea21b30242c7cc900fe10e252c58
Taz Uddin <tazuddinleton@gmail.com>
2023-05-15 09:54:43 +0600

    Add to source control

---
+++ src/main/java/com/example/movie_rental/domain/Actor.java
@@ -0,0 +1,26 @@
+package com.example.movie_rental.domain;
+
+public class Actor extends BaseEntity {
+    private String firstName;
+    private String lastName;
+
+    public Actor() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/resources/db/migration/V1_0__Add_Actor_Table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE IF NOT EXISTS actor (
+    id serial PRIMARY KEY,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL
+)
